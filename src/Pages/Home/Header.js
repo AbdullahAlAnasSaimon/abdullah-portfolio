0f8c0f52050f84dc1c8c2156f63fb7210cc90204
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-12-10 15:51:16 +0600

    applied google font to the site

--- src/Pages/Home/Header.js
+++ src/Pages/Home/Header.js
@@ -5,7 +5,7 @@ import { HiFolder, HiHome } from 'react-icons/hi';
 
 const Header = () => {
   return (
-    <div className='w-11/12 mx-auto'>
+    <div className='w-full md:w-11/12 mx-auto'>
       <div className="navbar bg-base-100">
         <div className="navbar-start">
           <div className="dropdown">
@@ -13,15 +13,15 @@ const Header = () => {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-auto">
-              <li><Link><HiHome /></Link></li>
-              <li><Link><TiUser /></Link></li>
-              <li><Link><HiFolder /></Link></li>
+              <li className='mb-2'><NavLink to='/'><HiHome className='text-xl' /></NavLink></li>
+              <li className='mb-2'><NavLink to='/about'><TiUser className='text-xl' /></NavLink></li>
+              <li><NavLink to='/projects'><HiFolder className='text-xl' /></NavLink></li>
             </ul>
           </div>
           <div className='hidden lg:flex'>
             <ul className="menu menu-horizontal px-1">
-              <li><NavLink to='/'><HiHome className='text-2xl' /></NavLink></li>
-              <li><NavLink to='/about'><TiUser className='text-2xl' /></NavLink></li>
+              <li className='mr-3'><NavLink to='/'><HiHome className='text-2xl' /></NavLink></li>
+              <li className='mr-3'><NavLink to='/about'><TiUser className='text-2xl' /></NavLink></li>
               <li><NavLink to='/projects'><HiFolder className='text-2xl' /></NavLink></li>
             </ul>
           </div>
