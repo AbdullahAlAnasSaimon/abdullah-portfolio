12d5b2ba198cbf3efb443cbd765ddbc468813470
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-12-10 15:43:56 +0600

    created routers and pages

---
+++ src/Pages/Home/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { TiUser } from 'react-icons/ti';
+import { HiFolder, HiHome } from 'react-icons/hi';
+
+const Header = () => {
+  return (
+    <div className='w-11/12 mx-auto'>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-auto">
+              <li><Link><HiHome /></Link></li>
+              <li><Link><TiUser /></Link></li>
+              <li><Link><HiFolder /></Link></li>
+            </ul>
+          </div>
+          <div className='hidden lg:flex'>
+            <ul className="menu menu-horizontal px-1">
+              <li><NavLink to='/'><HiHome className='text-2xl' /></NavLink></li>
+              <li><NavLink to='/about'><TiUser className='text-2xl' /></NavLink></li>
+              <li><NavLink to='/projects'><HiFolder className='text-2xl' /></NavLink></li>
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-end">
+          <Link className="btn">Get started</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
