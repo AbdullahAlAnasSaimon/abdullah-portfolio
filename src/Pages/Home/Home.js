c6201ceec63726926316e497ddfd2f3a7a91fddc
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-12-15 01:48:43 +0600

    finalize for now

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,13 +6,13 @@ import { FaFacebook, FaGithub, FaLinkedin } from 'react-icons/fa';
 const Home = () => {
   return (
     <div className='flex hero-content w-11/12 mx-auto'>
-      <div>
-        <ul>
-          <li><a href="https://www.facebook.com/abdullahalanassaimon/" target='_blank'><FaFacebook className='text-2xl text-blue-500 hidden lg:block' /></a></li>
-          <li><a href="https://www.linkedin.com/in/abdullahalanassaimon/" target='_blank'><FaLinkedin className='text-2xl text-blue-700 hidden lg:block' /></a></li>
-          <li><a href="https://github.com/AbdullahAlAnasSaimon/" target='_blank'><FaGithub className='text-2xl text-slate-700 hidden lg:block' /></a></li>
+      <div className='w-[25px] hidden lg:block'>
+        <ul className='text-center h-[230px] w-[25px]'>
+          <li><a href="https://www.facebook.com/abdullahalanassaimon/" target='_blank'><FaFacebook className='text-2xl text-blue-500 hidden lg:block text-center my-4' /></a></li>
+          <li><a href="https://www.linkedin.com/in/abdullahalanassaimon/" target='_blank'><FaLinkedin className='text-2xl text-blue-700 hidden lg:block my-4' /></a></li>
+          <li><a href="https://github.com/AbdullahAlAnasSaimon/" target='_blank'><FaGithub className='text-2xl text-slate-500 hidden lg:block my-4' /></a></li>
         </ul>
-        <p className='rotate-[270deg]'>Follow Me On</p>
+        <p className='rotate-[270deg] whitespace-nowrap font-bold'>Follow Me On</p>
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 hero-content h-full md:!h-screen'>
         <div className='text-center md:text-left my-10 md:my-0'>
@@ -29,6 +29,11 @@ const Home = () => {
             />
           </div>
           <a href='https://drive.google.com/file/d/1-640XGzasvFDojVoQLQPqJXKLfH_emQS/view?usp=share_link' target='_blank' rel="noreferrer" download><button className='rounded-full border-2 bg-slate-700 hover:bg-slate-100 hover:text-slate-700 hover:border-slate-700 text-white px-4 py-2 text-md duration-300 mt-5' >Download Resume</button></a>
+          <div className='block md:hidden my-7'>
+            <a href="https://www.facebook.com/abdullahalanassaimon/" target='_blank'><FaFacebook className='text-2xl text-blue-500 text-center inline-block' /></a>
+            <a href="https://www.linkedin.com/in/abdullahalanassaimon/" target='_blank'><FaLinkedin className='text-2xl text-blue-700 mx-4 inline-block' /></a>
+            <a href="https://github.com/AbdullahAlAnasSaimon/" target='_blank'><FaGithub className='text-2xl text-slate-500 inline-block' /></a>
+          </div>
         </div>
         <div>
           <img src={myImg} alt="" className='mx-auto block w-96 md:w-auto border-4 dark:bg-white light:bg-slate-500 rounded-full' />
